b610ea49a75b3c05f12462a9bcf5791611e0d341
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-01-08 08:19:07 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)  // different syntax to use "alert" inside node and we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability should be high dont do this, use proper intendation
+
+console.log("Ashish")
