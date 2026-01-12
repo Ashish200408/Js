30bf3a855a230785139319590484beca9965cf6a
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-01-12 13:11:46 +0000

    done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,3 +7,29 @@ console.log(3
     3)  // code readability should be high dont do this, use proper intendation
 
 console.log("Ashish")
+
+let name = "Ashish"
+let age = 21
+let isloggedIn = false
+
+
+// number => 2 to power 53
+// bigint for larger numbers 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => uniqueness 
+
+
+// object
+
+console.log(typeof undefined); // undefined as output 
+console.log(typeof null); // object as output
+console.log(typeof "Ashish"); // string as output 
+console.log(typeof age); // number as output
+
+
+
+
+
