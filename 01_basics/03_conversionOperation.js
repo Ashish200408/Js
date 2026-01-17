025a375d440002b1321001711a8e5f2828f3a395
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-01-17 13:06:41 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,7 +25,60 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber); 
+// console.log(stringNumber);
+// console.log(typeof stringNumber); 
+
+// anything that comes under "" is string 
+
+
+// *********************** Operations **********************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  // power
+// console.log(2/3);
+// console.log(2%2);  // reminder
+
+let str1 = "hello"
+let str2 = " Ashish"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // if input is string takes all as string and output is 122
+// console.log(1 + 2 + "2"); // if not string it preforms the addition and gives output as 32
+
+// console.log( (3 + 4) * 5 % 3); // always use brackets to make code easy to read and execute
+
+
+// console.log(+true);
+// console.log(+""); // not preffered 
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 // dont do like this 
+
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter; // prefix and postfix read from mdn and if anything more to study 
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
 
 
